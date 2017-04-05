e207f08d4c7aba655ede45818bec0718fb9e668e
Teddy Rendahl <trendahl@slac.stanford.edu>
2017-04-05 11:31:24 -0700

    Setuptools and versioneer

---
+++ setup.py
@@ -0,0 +1,13 @@
+import versioneer
+from setuptools import (setup, find_packages)
+
+setup(name     = 'hxdhome',
+      version  = versioneer.get_version(),
+      cmdclass = versioneer.get_cmdclass(),
+      license  = 'BSD',
+      author   = 'SLAC National Accelerator Laboratory',
+
+      packages    = find_packages(),
+      description = 'Basic Access to HXR Devices',
+
+    )
