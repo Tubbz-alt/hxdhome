733fa1e1bb1c47d57ef9bf10e86756fc579f08ca
Teddy Rendahl <trendahl@slac.stanford.edu>
2017-04-05 11:24:11 -0700

    Basic package and testing setup

---
+++ tests/conftest.py
@@ -0,0 +1,15 @@
+############
+# Standard #
+############
+import logging
+
+###############
+# Third Party #
+###############
+import happi.tests
+
+##########
+# Module #
+##########
+
+logger = logging.getLogger(__name__)
