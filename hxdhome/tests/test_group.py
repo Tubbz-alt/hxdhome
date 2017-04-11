c993e0fc3f08de43611992ef472feb4d18f75ec9
Teddy Rendahl <trendahl@slac.stanford.edu>
2017-04-10 23:02:39 -0700

    Core functionality fo group test

---
+++ hxdhome/tests/test_group.py
@@ -0,0 +1,36 @@
+############
+# Standard #
+############
+
+###############
+# Third Party #
+###############
+from happi import Device
+
+##########
+# Module #
+##########
+from hxdhome import HXDGroup
+
+
+def test_alias():
+    g = HXDGroup(name='DG2 TEST')
+    assert g.alias == 'dg2test'
+
+
+def test_device_handling():
+    sub_d  = Device(name='sub_device')
+    main_d = Device(name='main_device')
+    sub  = HXDGroup(sub_d, name='sub')
+    main = HXDGroup(main_d, sub, name='main')
+    assert main.children  == (main_d, sub)
+    assert main.devices   == [main_d, sub_d]
+    assert main.subgroups == [sub]
+    assert sub.subgroups  == []
+
+
+def test_pv():
+    sub_1  = HXDGroup(name='sub_1')
+    sub_2  = HXDGroup(name='sub_2')
+    main   = HXDGroup(sub_1, sub_2, name='main')
+    assert str(main.pv) == 'LOC\\main=e:0,sub_1,sub_2'
