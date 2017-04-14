5485f0dd711c665b60377d13606b021801d556c1
Teddy Rendahl <trendahl@slac.stanford.edu>
2017-04-14 16:12:50 -0700

    No longer neccesary to have destroy_on_exit

--- hxdhome/utils.py
+++ hxdhome/utils.py
@@ -1,11 +1,10 @@
 """
-Basic utilities for
+Basic utilities for hxdhome
 """
 ############
 # Standard #
 ############
-import logging
-import threading
+
 ###############
 # Third Party #
 ###############
@@ -15,16 +14,3 @@ import threading
 # Module #
 ##########
 
-
-def destroy_on_exit(proc, tmp):
-    """
-    Destory temporary files on process completion
-    """
-    #Worker
-    def wait(proc, tmp):
-        proc.wait()
-        list(map(lambda x : x.close(), tmp))
-    #Run
-    thread = threading.Thread(target=wait, args=(proc, tmp))
-    thread.start()
-    return thread
